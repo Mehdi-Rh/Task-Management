d3bf63378009118ba9833aca44d3ec413ea9a9c0
Mehdi Rahal <mehdi@lablabee.com>
2024-04-20 20:46:44 +0100

    Add name to user

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,6 +5,11 @@ const validator = require("validator");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   email: {
     type: String,
     required: true,
@@ -17,7 +22,7 @@ const userSchema = new Schema({
 });
 
 // static signup method
-userSchema.statics.signup = async function (email, password) {
+userSchema.statics.signup = async function (name, email, password) {
   // validation
   if (!email || !password) {
     throw Error("All fields must be filled");
@@ -38,13 +43,13 @@ userSchema.statics.signup = async function (email, password) {
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
 
-  const user = await this.create({ email, password: hash });
+  const user = await this.create({ name, email, password: hash });
 
   return user;
 };
 
 // static login method
-userSchema.statics.login = async function (email, password) {
+userSchema.statics.login = async function (name, email, password) {
   if (!email || !password) {
     throw Error("All fields must be filled");
   }
